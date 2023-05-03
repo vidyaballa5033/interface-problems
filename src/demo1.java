7ce1e929c8b691bb3eb635ef327f331384d79513
vidyaballa <vidyaballa503@gmail.com>
2023-05-03 22:09:31 +0530

    interface practice

--- src/demo1.java
+++ src/demo1.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class demo1 {
+
+public class demo1 {
+    static void display(int a){
+        System.out.println("The value is "+a);
+    }
+    static void display(int a , int b){
+        System.out.println("The value is "+a + " " + b) ;;
+    }
+    public static void main(String[] args) {
+        display(1);
+        display(1,4);
+        Language l1 = new Language();
+        Language l2 = new Language("Something");
+        l1.getLanguage();
+        l2.getLanguage();
+    }
+}
+class Language{
+    String language;
+    Language(){
+        this.language = "English";
+    }
+    Language(String s){
+        this.language = "Hindi";
+    }
+
+    public void getLanguage() {
+        System.out.println(language);
+    }
 }
